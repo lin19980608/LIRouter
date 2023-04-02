cf6c71c424b0958aa10c1d9fe410d730cd5187c9
mangfung <1309560323@qq.com>
2023-04-02 14:44:09 +0800

    Initial Commit

---
+++ LIRouter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LIRouter
+//
+//  Created by 林文峰 on 2023/4/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
