550d424d894cb0c71c25f6ef03404278ddd16d84
mangfung <1309560323@qq.com>
2023-04-02 15:48:46 +0800

    注释

--- LIRouter/WFRouter.swift
+++ LIRouter/WFRouter.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 @objc class LIRouter: NSObject {
-    fileprivate var routerMap:[String:AnyClass] = [:]
+    /// 单例
     public static let shared = LIRouter()
+    /// 路由映射字典
+    fileprivate var routerMap:[String:AnyClass] = [:]
+    /// 跳转方法
     fileprivate static let resolveSelector = NSSelectorFromString("jumpWithProps:")
+    /// 注册方法
     fileprivate static let registerSelector = NSSelectorFromString("registerWithRouter:")
 }
 
 extension LIRouter {
+    /// 注册方法
+    /// - Parameters:
+    ///   - url: 注册路径
+    ///   - className: 路径对应控制器类名
     public func register(url:String,className:AnyClass) {
         if self.routerMap[url] == nil {
             self.routerMap.updateValue(className, forKey: url)
@@ -23,6 +31,11 @@ extension LIRouter {
             assert(false,"\(url) 重复注册")
         }
     }
+    /// 跳转方法
+    /// - Parameters:
+    ///   - url: 跳转路径
+    ///   - props: 跳转参数
+    /// - Returns: 跳转结果
     @discardableResult
     public func resolve(url:String,props:[String:Any]) -> Bool {
         guard let anyClass = self.routerMap[url] else {
@@ -36,6 +49,7 @@ extension LIRouter {
         anyClass.perform(LIRouter.resolveSelector,with:props,afterDelay: 0)
         return true
     }
+    /// App启动后调用一次
     public func autoRegister() {
         for className in OCUtils.getAllClassName() {
             if let anyClass = NSClassFromString(className),
@@ -45,6 +59,9 @@ extension LIRouter {
         }
     }
 }
+
+
+
 extension UIViewController {
     class func currentViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         if let nav = base as? UINavigationController {
